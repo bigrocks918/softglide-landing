ff3baf54d261956bfc4c04d07da24062f52d3f6c
bigrocks918 <bigrocks918geek@gmail.com>
2024-11-15 12:32:06 +0900

    extracted breadcrumbs section as component from every page except index.tmpl

--- main.go
+++ main.go
@@ -24,42 +24,33 @@ func main() {
 
 	router.GET("/about", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "main/about.tmpl", gin.H{
-			"Title": "About Us"})
+			"Title": "About Us", "Img": "hero1"})
 	})
 
 	router.GET("/service", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "main/service.tmpl", gin.H{
-			"Title": "Services"})
+			"Title": "Services", "Img": "hero2"})
 	})
 
 	router.GET("/industries", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "main/industries.tmpl", gin.H{
-			"Title": "Industries"})
+			"Title": "Industries", "Img": "hero3"})
 	})
 
 	router.GET("/contact", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "main/contact.tmpl", gin.H{
-			"Title": "Contact Us"})
+			"Title": "Contact Us", "Img": "hero2"})
 	})
 
 	router.GET("/career", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "main/career.tmpl", gin.H{
-			"Title": "Career"})
+			"Title": "Career", "Img": "overlay"})
 	})
 
 	router.GET("/blog", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "main/blog.tmpl", gin.H{
-			"Title": "Blogs"})
+			"Title": "Blogs", "Img": "overlay"})
 	})
 
-	// router.GET("/subfolder1/page1", func(c *gin.Context) {
-	// 	c.HTML(http.StatusOK, "subfolder1/page1.html", nil)
-	// })
-
-	// router.GET("/subfolder2/page2", func(c *gin.Context) {
-	// 	c.HTML(http.StatusOK, "subfolder2/page2.html", nil)
-	// })
-
-	// Start the server
 	router.Run(":8080")
 }
